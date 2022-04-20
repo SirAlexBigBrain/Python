1313ece27f25b953108b153eaa9cffbf84a69233
Α Αλέξανδρος Ω <98426972+SirAlexBigBrain@users.noreply.github.com>
2022-04-20 14:16:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ if 7 < 9:
   print("Seven is less than nine!")
 if -3 > -4:
   print("Negative three is greater than negative four!")
-y = 5
-x = 2
-z = (x + y)
-echo ("The answer is z!")
+y = "Technodot"
+x = "Hardwaregore"
+print(x)
+print(y)
