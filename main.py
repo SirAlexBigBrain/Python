ec26d9258ce1042b041ad96e702b2dc9e55af9cd
Α Αλέξανδρος Ω <98426972+SirAlexBigBrain@users.noreply.github.com>
2022-04-20 18:43:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -52,3 +52,12 @@ if "fencer" in txt:
   print(10 > 9)
 print(10 == 9)
 print(10 < 9)
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+Try it Yourself »
