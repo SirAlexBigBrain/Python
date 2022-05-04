29e9ce6f945dd0678c3a0cac8cb87816617c82ab
Α Αλέξανδρος Ω <98426972+SirAlexBigBrain@users.noreply.github.com>
2022-05-04 17:18:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -139,3 +139,31 @@ else:
         
   people = "Wenqian and Yabo!
         f"I like {people}"
+
+  import turtle
+        
+  import random
+        
+  stamp = turtle.Turtle()
+        
+  stamp.shape('turtle')
+        
+  stamp.penup()
+        
+  turtle.colormode(255)
+        
+  paces = 20
+  random_red = 50
+  random_green = 50
+  random_blue = 50
+        
+        
+  for i in range(50):
+        random_red = random.randint(0, 255)
+        random_green = random.randint(0, 255)
+        random_blue = random.randint(0, 255)
+        stamp.color(random_red, random_green, random_blue)
+        stamp.stamp()
+        paces += 3
+        stamp.forward(paces)
+        stamp.right(25)
