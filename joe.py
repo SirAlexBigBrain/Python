6398a1f37816fcfd1a2922ccd9df1d91d800ce65
Α Αλέξανδρος Ω <98426972+SirAlexBigBrain@users.noreply.github.com>
2022-05-09 11:51:51 -0500

    Create joe.py

---
+++ joe.py
@@ -0,0 +1,170 @@
+print ("hello, world!")
+if 7 < 9:
+  print("Seven is less than nine!")
+if -3 > -4:
+  print("Negative three is greater than negative four!")
+y = "Technodot"
+x = "Hardwaregore"
+print(x)
+print(y)
+my_variable = 'Techdudie'
+print("my_variable is the best!")
+a, b, = ['TechnoDot', 'Hardwaregore']
+print(a)
+print(b)
+Names = ["@SirAlexBigBrain", "@TechnoDot", "@Hardwaregore"]
+x, y, z = Names
+print(x)
+print(y)
+print(z)
+x = "love video games!!!"
+print(x)
+def myfunc():
+  print("Hardwaregore and TechnoDot are " + x)
+  
+  myfunc()
+  
+Alex = 1234567890"
+Q = "2.12"
+Z = 67j"
+
+print(int(Alex))
+print(float(Q))
+print(complex(Z))
+
+import random
+
+print(random.randrange(-18, 36))
+
+txt = "I am a fencer!"
+if "fencer" in txt:
+  print("Yes, 'fencer' is present.")
+
+  a = "I Play Soccer!"
+  print(a.upper())
+
+  a = "I am TechnoDot!"
+  print(a.replace("TechnoDot", "SirAlexBigBrain"))
+
+  s = w
+  print(capitalize(s))	
+
+  print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+  x = 4
+  y = 3
+  print(x**y)
+
+  mylist = ["A", "W", "Y"]
+print(mylist)
+
+thislist = ["a", "b", "c"]
+print(len(thislist))
+
+thislist = ["QwErTy", "QWERTY", "qwerty"]
+i = 0
+while i < len(thislist):
+  print(thislist[i])
+  i = i + 1
+
+  list1 = ["@Al", "@We", "@Ya"]
+  list1.sort()
+  print(list1)
+
+  list2 = [100, 50, 65, 82, 23]
+list2.sort()
+print(list2)
+
+thisdict = {
+  "brand": "EA fencing",
+  "model": "Pistol Grip",
+  "year": 2021
+}
+print(thisdict)
+
+a = 23140
+b = 33333
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+  
+  print(a / b)
+
+  i = 1
+while i < 200:
+  print(i)
+  i += 1
+
+   x in range(6):
+  print(x)
+else:
+  print("Whew! I'm done!")
+
+  def my_function():
+    print("Python is so much more efficent! " + a - b)
+    
+    
+    print(my_funtion())
+
+  def my_function(*language):
+  print("The better coding language is " + language[1])
+
+  my_function("Bash", "Python")
+
+  people = ["@YOU", "@ME", "@PERSON OVER THERE"]
+  print(people)
+
+  people[0] = "not you, Y"
+  
+  people.remove("@ME")
+
+  mytuple = ("@SirAlexBigBrain", "@TechnoDot", "@Hardwaregore")
+  myit = iter(mytuple)
+  print(next(myit))
+  print(next(myit))
+  print(nex:
+        
+  people = "Wenqian and Yabo!
+        f"I like {people}"
+
+  import turtle
+        
+  import random
+        
+  stamp = turtle.Turtle()
+        
+  stamp.shape('turtle')
+        
+  stamp.penup()
+        
+  turtle.colormode(255)
+        
+  paces = 20
+  random_red = 50
+  random_green = 50
+  random_blue = 50
+        
+        
+  for i in range(50):
+        random_red = random.randint(0, 255)
+        random_green = random.randint(0, 255)
+        random_blue = random.randint(0, 255)
+        stamp.color(random_red, random_green, random_blue)
+        stamp.stamp()
+        paces += 3
+        stamp.forward(paces)
+        stamp.right(25)
+        
